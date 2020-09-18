8b465aca495fc231dc6d95b6bb01c3b5748403e4
Steveen <steveen.echeverri@gmail.com>
2020-09-18 18:45:49 -0500

    Created queries users controllers test

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,10 +1,21 @@
 const userCtr = {};
 const User = require('../models/User');
 
-userCtr.getUsers = (req, res) => res.send('User Router');
+userCtr.getUsers = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+};
 
-userCtr.createUser = (req, res) => res.send('Create User Router');
+userCtr.createUser = async (req, res) => {
+    const { username } = req.body;
+    const newUser = new User({username});
+    await newUser.save();
+    res.json('Create User Router')
+};
 
-userCtr.deleteUser = (req, res) => res.send('Delete User Router');
+userCtr.deleteUser = (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json('Delete User')
+};
 
 module.exports = userCtr;
