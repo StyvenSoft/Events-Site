f344102235892513553dae3ce71a7a74c10d33a4
Steveen <steveen.echeverri@gmail.com>
2020-09-18 21:46:22 -0500

    Initial frontend practical project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          <code>Initial frontend practical project</code> 
+        </p>
+    </div>
+  );
+}
+
+export default App;
