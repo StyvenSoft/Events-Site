cb8016868d7f1ec85865c85eda4eaf1e4ea59feb
Steveen <steveen.echeverri@gmail.com>
2020-09-18 22:53:23 -0500

    Import components in router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from './components/Navigation';
+import EventsList from './components/EventsList';
+import CreateEvent from './components/CreateEvent';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          <code>Initial frontend practical project</code> 
-        </p>
-    </div>
+    <Router>
+      <Navigation></Navigation>
+      <Route path="/" exact component={EventsList} />
+      <Route path="/edit/:id" component={CreateEvent} />
+      <Route path="/create" component={CreateEvent} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
