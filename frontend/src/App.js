4db85dbbab2262310f1c372b0cb58a82658b641f
Steveen <steveen.echeverri@gmail.com>
2020-09-19 16:00:50 -0500

    Created navigation router bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from './components/Navigation';
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">Event App</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/">Events</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/create">Create Event</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user">Create User</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         )
     }
 }
