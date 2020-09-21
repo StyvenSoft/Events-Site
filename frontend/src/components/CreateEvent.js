de89c2c52bf240a4458b366dd358222d4c3e3bfd
Steveen <steveen.echeverri@gmail.com>
2020-09-21 12:50:53 -0500

    Initial create form in CreateEvent

--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class CreateEvent extends Component {
+
+    onSubmit = e => {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                CreateEvent
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a Event</h4>
+                    <form onSubmit={this.onSubmit}>
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </form>
+                </div>
             </div>
         )
     }
