8dc86c6bed1755c9d16f77afc3c7af67b1596d2e
Steveen <steveen.echeverri@gmail.com>
2020-09-21 16:46:38 -0500

    Instance userSelected for create event form

--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class CreateEvent extends Component {
+
+    state = {
+        users: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({ users: res.data.map(user => user.username) });
+    }
 
     onSubmit = e => {
         e.preventDefault();
@@ -11,6 +21,18 @@ export default class CreateEvent extends Component {
             <div className="col-md-6 offset-md-3">
                 <div className="card card-body">
                     <h4>Create a Event</h4>
+                    <div className="form-group">
+                        <select name="userSelected"
+                            className="form-control"
+                        >
+                            {
+                                this.state.users.map(user =>
+                                    <option key={user}>
+                                        {user}
+                                    </option>)
+                            }
+                        </select>
+                    </div>
                     <form onSubmit={this.onSubmit}>
                         <button type="submit" className="btn btn-primary">Save</button>
                     </form>
