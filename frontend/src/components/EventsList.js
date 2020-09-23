29645e3d643c4eab0cee8b89c86eef09f5d79079
Steveen <steveen.echeverri@gmail.com>
2020-09-22 19:51:24 -0500

    Implement first eventList components axios.get

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class EventsList extends Component {
+
+    state = {
+        events: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/events')
+        console.log(res);
+        this.setState({events: res.data})
+    }
+
     render() {
         return (
-            <div>
-                EventsList
+            <div className="row">
+                {
+                    this.state.events.map(event => (
+                        <div className="col-md-4 p-2" key={event._id}>
+                            <div className="card">
+                                <div className="card-body">
+                                    <p>{event.content}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
